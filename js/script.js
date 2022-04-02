92cc31384844d2dd18b4350b9da3bcb79517cf6a
mrslvs <sekerka.miroslav@outlook.com>
2022-04-02 02:26:34 +0200

    added first functions to js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,106 @@
-///////////////////////////////////////////////////////////
+// ======= add current year to footer =======
+// ==========================================
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// ======= mobile navigation =======
+// =================================
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// ======= smooth scrolling animation =======
+// ==========================================
+
+// add this link to html in order to make this work on safari browser: https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // when the link is clicked:
+    e.preventDefault();
+
+    //get the attribute where the link points to (e.g.: #cta)
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      // if the attribute is "#" (link points to top of the page):
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      // if it doesn't point to the top but elsewhere, scroll there:
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      // if the link that has been clicked is from the mobile navigation:
+      // close the navigation menu
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+// ========= sticky navigation bar =========
+// =========================================
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    // array of entries for each threshold value
+    // now there is only one threshold value
+    const ent = entries[0];
+
+    // when the hero section moves out of the viewport
+    // => value FALSE is set to attribute "isIntersection" of ent
+    if (ent.isIntersecting === false) {
+      //html structure:
+      // body
+      //    header
+      //    main
+      //      section-hero
+
+      // add class sticky to the BODY, because we want to add margin to .section-hero to fix screen jump
+      // in order to add margin to section-hero, we need to check if sticky has been applied to parent element (body)
+      document.body.classList.add("sticky");
+    }
+
+    //remove it once the section-hero is in viewport again
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // defining options inside of this "object {}":
+
+    // "should this element be apperaing or not?"
+    // => we observe hero section in the viewport
+    root: null,
+
+    // event happens once the hero section has none of it inside the viewport
+    threshold: 0,
+    rootMargin: "-80px", //height of sticky nav
+  }
+);
+obs.observe(sectionHeroEl);
+
+//-------------------------------------------------------------
+//---------------- included in starter js file ----------------
+//-------------------------------------------------------------
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
@@ -17,8 +119,6 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
